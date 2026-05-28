073f3eef9c00e6beb4a476986cb519e107ca8022
Brennan Fitzgerald <bfitzger@login3.talapas.uoregon.edu>
2026-05-27 23:36:02 -0700

    cleaned up update

--- data_loader/my_dataloader.py
+++ data_loader/my_dataloader.py
@@ -10,17 +10,21 @@ class EnsDataLoader(DataLoader):
     """
     EnsDataLoader that uses the pytorch geometric dataloader as its base
     """
-    def __init__(self, ens_dirs, batch_size, shuffle=False, num_workers=1, training=True):
+    def __init__(self, ens_dirs, batch_size, struct_num, shuffle=False, num_workers=1, training=True):
 
         trsfm = transforms.Compose([
             transforms.Normalize((-0.5,), (0.5,))
         ])
         self.ens_dirs = ens_dirs
 
-        self.struct1, self.struct2,ddg_df=combining_ensembles(ens_dirs)
+        if struct_num==1:
+            self.struct, placeholder,ddg_df=combining_ensembles(ens_dirs)
+            
+        if struct_num==2:
+            placeholder, self.struct,ddg_df=combining_ensembles(ens_dirs)
         
         self.labels=ddg_df.max_diff_ddg
         
-        self.dataset = load_geom_data(self.struct1)
+        self.dataset = load_geom_data(self.struct)
         
-        super().__init__(self.dataset1, batch_size, shuffle, num_workers)
+        super().__init__(self.dataset, batch_size, shuffle, num_workers)
